22849943ab497333bd3de0568f1da9438311b3c1
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-10-23 17:38:47 +0900

    Add files via upload
    BFS, DFS

---
+++ LJS01-01/LJS01-01/LJS01-01.cpp
@@ -0,0 +1,12 @@
+﻿// LJS01-01.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include "graph.h"
+
+int main()
+{
+	C_GRAPH cGraph{};
+	//cGraph.DFS(1);
+	cGraph.BFS(1);
+}
