5a6c0e5c6c8a48d678a4cd7e7c75790e54932e79
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-11-18 17:47:35 +0900

    Add files via upload
    다리놓기 백준

---
+++ LJS01-06-Bridge/LJS01-06-Bridge/bridge.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <list>
+
+class C_BRIDGE
+{
+private:
+	struct S_NODE
+	{
+		bool bLand;
+		std::list<S_NODE*> child;
+		std::list<S_NODE*>::iterator iterNode;
+		bool bVisit;
+		int nTest;
+	};
+private:
+	int	m_arLand[10][10];
+	S_NODE m_arNode[10][10];
+	std::list<S_NODE*> m_listNode;
+	std::list< std::list<S_NODE*>* > m_listLand;
+
+private:
+	void findLand(S_NODE* pNode, std::list<S_NODE*>* pLand);
+	void Print();
+public:
+	C_BRIDGE();
+	void makeNode();
+	void run();
+};
---
+++ bridge/bridge/bridge.cpp
@@ -0,0 +1,89 @@
+#include "bridge.h"
+
+
+C_BRIDGE::C_BRIDGE() : m_arLand
+	{
+	{1,1,1,0,0,0,0,1,1,1},
+	{1,1,1,1,0,0,0,0,1,1},
+	{1,0,1,1,0,0,0,0,1,1},
+	{0,0,1,1,1,0,0,0,0,1},
+	{0,0,0,1,0,0,0,0,0,1},
+	{0,0,0,0,0,0,0,0,0,1},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,1,1,0,0,0,0},
+	{0,0,0,0,1,1,1,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0}
+	},
+	m_arNode{}
+{
+}
+
+void C_BRIDGE::makeNode()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			m_arNode[i][j].bLand = m_arLand[i][j];
+			//m_listNode.push_front(&m_arNode[i][j]);
+			//m_arNode[i][j].iterNode = m_listNode.begin();
+			m_listNode.push_back(&m_arNode[i][j]);
+			auto iter = m_listNode.end();
+			iter--;
+			m_arNode[i][j].iterNode = iter;
+		}
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			int arMove[4][2]{ {-1,0},{1,0},{0,-1},{0,1} };
+			for (int k = 0; k < 4; k++)
+			{
+				int nY = i + arMove[k][0];
+				int nX = j + arMove[k][1];
+				if(nY >= 0 && nY < 10 && nX >= 0 && nX < 10)
+					m_arNode[i][j].child.push_back(&m_arNode[nY][nX]);
+			}
+		}
+	}
+}
+
+void C_BRIDGE::findLand(S_NODE* pNode , std::list<S_NODE*>* pLand)
+{
+	if (pNode->iterNode == m_listNode.end())
+		return;
+
+	pLand->push_back(pNode);
+	m_listNode.erase(pNode->iterNode);
+	pNode->iterNode = m_listNode.end();
+	for (S_NODE* pChild : pNode->child)
+	{
+		findLand(pChild, pLand);
+	}
+}
+
+void C_BRIDGE::run()
+{
+	auto iter = m_listNode.begin();
+	while(iter != m_listNode.end())
+	{
+		if (!(*iter)->bLand)
+		{
+			(*iter)->iterNode = m_listNode.end();
+			iter = m_listNode.erase(iter);
+		}
+		else
+			iter++;
+	}
+
+	while (!m_listNode.empty())
+	{
+		std::list<S_NODE*>* pLand = new std::list<S_NODE*>{};
+		findLand(*m_listNode.begin(), pLand);
+		m_listLand.push_back(pLand);
+	}
+
+
+}
